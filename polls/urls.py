cfa5f536e64f37f4d9a0d7024fcc938b0b2f3032
yurihasuike <odyssey.yh0204@gmail.com>
2017-08-31 11:36:21 +0900

    first

--- polls/urls.py
+++ polls/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name="polls"
 urlpatterns = [
-    url(r'(?P<pk>\d+)/$', views.detail, name='poll_detail'),
+    url(r'(?P<pk>\d+)/$', views.detail, name='polls_detail'),
     url(r'(?P<pk>\d+)/vote$', views.vote, name='poll_vote'),
-    url(r'(?P<pk>\d+)/results$', views.results, name='poll_results'),
+    url(r'(?P<pk>\d+)/results$', views.results, name='polls_results'),
     url(r'^$',views.index,name='index'),
     url(r'^form$', views.form_test),
 ]
--- polls/views.py
+++ polls/views.py
@@ -25,15 +25,14 @@ def vote(request,pk):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError,Choice.DoesNotExist):
-        return render(request,'poll/detail.html',{
+        return render(request,'polls/detail.html',{
             'question':question,
             'error_message':"You didn't select a choice",
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-    return redirect(reverse('polls:poll_results'), pk=pk)
-    # return redirect('results_url', pk=pk)
+    return redirect('polls:polls_results', pk=pk)
 
 
 def results(request,pk):
